759665963d1553297d403c35cb8846ef1850ffc4
RockyAo <yun.ao@grabtaxi.com>
2018-07-19 18:23:55 +0800

    值语义

--- linked-list.playground/Contents.swift
+++ linked-list.playground/Contents.swift
@@ -46,6 +46,7 @@ public struct LinkedList<Value> {
     }
     
     public mutating func push(_ value: Value) {
+        copyNodes()
         head = Node(value: value, next: head)
         if tail == nil {
             tail = head
@@ -53,6 +54,7 @@ public struct LinkedList<Value> {
     }
     
     public mutating func append(_ value: Value) {
+        copyNodes()
         guard !isEmpty else {
             push(value)
             return
@@ -62,6 +64,7 @@ public struct LinkedList<Value> {
     }
     
     public func node(at index: Int) -> Node<Value>? {
+        
         var currentNode = head
         var currentIndex = 0
         
@@ -75,6 +78,7 @@ public struct LinkedList<Value> {
     
     @discardableResult
     public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+        copyNodes()
         guard tail !== node else {
             append(value)
             return tail!
@@ -86,6 +90,7 @@ public struct LinkedList<Value> {
     
     @discardableResult
     public mutating func pop() -> Value? {
+        copyNodes()
         defer {
             head = head?.next
             if isEmpty {
@@ -98,6 +103,7 @@ public struct LinkedList<Value> {
     
     @discardableResult
     public mutating func removeLast() -> Value? {
+        copyNodes()
         guard let head = head else {
             return nil
         }
@@ -121,6 +127,7 @@ public struct LinkedList<Value> {
     
     @discardableResult
     public mutating func remove(after node: Node<Value>) -> Value? {
+        copyNodes()
         defer {
             if node.next === tail {
                 tail = node
@@ -128,6 +135,23 @@ public struct LinkedList<Value> {
             node.next = node.next?.next
         }
         return node.next?.value
+    }
+    
+    private mutating func copyNodes() {
+        guard var oldNode = head else {
+            return
+        }
+        
+        head = Node(value: oldNode.value)
+        var newNode = head
+        
+        while let nextOldNode = oldNode.next {
+            newNode!.next = Node(value: nextOldNode.value)
+            newNode = newNode!.next
+            oldNode = nextOldNode
+        }
+        
+        tail = newNode
     }
 }
 
@@ -266,4 +290,18 @@ example(of: "using collection") {
     
     let sum = list.reduce(0, +)
     print("Sum of all values: \(sum)")
+}
+
+example(of: "linked list cow") {
+    var list1 = LinkedList<Int>()
+    list1.append(1)
+    list1.append(2)
+    var list2 = list1
+    print("List1: \(list1)")
+    print("List2: \(list2)")
+
+    print("After appending 3 to list2")
+    list2.append(3)
+    print("List1: \(list1)")
+    print("List2: \(list2)")
 }
